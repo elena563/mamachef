4d4e90f0958f84b8ca48c1bc7459f082241d7744
Elena Zen <elenazen563@gmail.com>
2026-08-08 15:16:01 +0200

    Add logging in settings

--- mamachef/settings.py
+++ mamachef/settings.py
@@ -152,3 +152,22 @@ INTERNAL_IPS = [
 LOGIN_REDIRECT_URL = 'Kitchen:home'
 LOGOUT_REDIRECT_URL = 'Kitchen:home'
 LOGIN_URL = 'Kitchen:login'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'django-errors.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
